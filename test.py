793cc1399213e4ed4a8ff606543508c23b9fb81e
er2q <1961879865@qq.com>
2022-08-24 11:55:26 +0800

    模型参数统计

--- test.py
+++ test.py
@@ -6,11 +6,41 @@
 # @software: PyCharm
 
 """
-文件说明：
+文件说明：模型大小
     
 """
-items = [1, 2, 3, 4]
-for idx, item in enumerate(items):
-    items.remove(item)
+import os.path as osp
+from glob import glob
+from mmcv import Config
 
-print(items)
+from mmocr.models import build_detector
+
+
+def get_model_size(model):
+    param_size = 0
+    param_sum = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+        param_sum += param.nelement()
+    # buffer_size = 0
+    # buffer_sum = 0
+    # for buffer in model.buffers():
+    #     buffer_size += buffer.nelement() * buffer.element_size()
+    #     buffer_sum += buffer.nelement()
+    all_size = (param_size) / 1024 / 1024       # K,MB
+    print('模型总大小为：{:.3f}MB'.format(all_size))
+    # return param_size, param_sum, buffer_size, buffer_sum, all_size
+
+
+if __name__ == '__main__':
+    for config_path in glob(osp.join('configs/textdet', '*/*.py')):
+        print(config_path)
+        cfg = Config.fromfile(config_path)
+        model = build_detector(
+            cfg.model,
+            train_cfg=cfg.get('train_cfg'),
+            test_cfg=cfg.get('test_cfg'))
+        get_model_size(model)
+        # total = sum([param.nelement() for param in model.parameters()])
+        #
+        # print("Number of parameter: %.2fM" % (total / 1024 / 1024))
